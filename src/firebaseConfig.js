bc0b499a62c3bd58192d676ae10d7983fabec118
suot00 <truongkhacsuot661@gmail.com>
2022-04-22 12:51:46 +0700

    update new firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD2jGeWtYvMdzVOsjcG7lJv_zpdu0pAXg0",
-  authDomain: "instagram-clone-react-f9d3f.firebaseapp.com",
-  projectId: "instagram-clone-react-f9d3f",
-  storageBucket: "instagram-clone-react-f9d3f.appspot.com",
-  messagingSenderId: "323214745376",
-  appId: "1:323214745376:web:c691c22f8b97ae46a50145",
-  measurementId: "G-LWP8T7BBGG",
+  apiKey: "AIzaSyDrN475ocQzcifbFmDJY9or-zOtkTNPXVg",
+  authDomain: "instagram-clone-react-9fa7e.firebaseapp.com",
+  projectId: "instagram-clone-react-9fa7e",
+  storageBucket: "instagram-clone-react-9fa7e.appspot.com",
+  messagingSenderId: "635006955970",
+  appId: "1:635006955970:web:c211e6580105febba5265b",
+  measurementId: "G-1FDRS2C5DC",
 });
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
